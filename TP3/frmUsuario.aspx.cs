41b0120480d6ad35a4df8e6dbbcd58d0f75bf561
Lucas De Marchi <131633835+demarchi13@users.noreply.github.com>
2024-04-20 13:16:41 -0300

    Segunda subida con comentarios

--- TP3/frmUsuario.aspx.cs
+++ TP3/frmUsuario.aspx.cs
@@ -9,6 +9,7 @@ namespace TP3
 {
     public partial class frmUsuario : System.Web.UI.Page
     {
+        //PUNTO 3
         protected void Page_Load(object sender, EventArgs e)
         {
             if(IsPostBack == false)
@@ -17,8 +18,10 @@ namespace TP3
             }
         }
 
+        //PUNTO 4
         public void GuardarLocalidad()
         {
+            //PUNTO 4.1
             string localidad = txtLocalidad.Text.Trim(); //Guarda el nombre de la localidad
             bool banderaLocalidad = false;
 
@@ -31,37 +34,43 @@ namespace TP3
                 }
             }
 
+            //PUNTO 4.2
             txtLocalidad.Text = ""; //Limpiar textbox
 
+            //PUNTO 4.3 Crear el if y el else y sol agregar la localidad
             if (!banderaLocalidad) //Si no es verdadero quiere decir que la localidad no existe y procede a agregar la localidad
             {
-                ddlLocalidades.Items.Add(new ListItem(localidad)); //Agregar localidad
-                lblLocalidadRepetida.ForeColor = System.Drawing.Color.Green; //Cambiar color al label
-                lblLocalidadRepetida.Text = "Localidad Agregada exitosamente";
+                ddlLocalidades.Items.Add(new ListItem(localidad)); //PUNTO 4.3 Agregar localidad
+                lblLocalidadRepetida.ForeColor = System.Drawing.Color.Green; //PUNTO 4.2 Cambiar color al label
+                lblLocalidadRepetida.Text = "Localidad Agregada exitosamente"; //PUNTO 4.2 Agregar mensaje
             }
             else
             {
-                lblLocalidadRepetida.ForeColor = System.Drawing.Color.Red;
-                lblLocalidadRepetida.Text = "*Esta localidad ya se encuentra registrada*";            
+                lblLocalidadRepetida.ForeColor = System.Drawing.Color.Red; //PUNTO 4.2
+                lblLocalidadRepetida.Text = "*Esta localidad ya se encuentra registrada*"; //PUNTO 4.2            
             }
         }
 
+        //PUNTO 5
         protected void btnGuardarLocalidad_Click(object sender, EventArgs e)
         {
             GuardarLocalidad();    
         }
 
+        //PUNTO 6 Crear el evento
         protected void btnGuardarUsuario_Click(object sender, EventArgs e)
         {
-            lblLocalidadRepetida.Text = ""; //Limpiar la casilla si se presiona el boton
+            lblLocalidadRepetida.Text = ""; //PUNTO 6 Limpiar la casilla si se presiona el boton
 
+            //PUNTO 6 Crear solo el if y comprobar que todas las validaciones de la pagina esten bien
             if (Page.IsValid)
             {
-                string usuario = txtUsuario.Text.Trim();
-                lblBienvenida.Text = "Bienvenido " + usuario;
+                string usuario = txtUsuario.Text.Trim(); //PUNTO 6.1 Guardar el nombre de usuario
+                lblBienvenida.Text = "Bienvenido " + usuario; //PUNTO 6.1 Mensaje
             }
         }
 
+        //PUNTO 7 Crear el vento que redirige al frm Bienvenida
         protected void btnInicioAspx_Click(object sender, EventArgs e)
         {
             Response.Redirect("frmBienvenida.aspx");
